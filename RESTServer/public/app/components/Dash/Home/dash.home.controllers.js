9b7a45c113a0e73cf99af716aa49c14d19595536
expovin <expovin@gmail.com>
2016-12-01 23:44:44 +0100

    Aggiunta controllo temperatura e grafico temperatura in pagina Home di Dash

--- RESTServer/public/app/components/Dash/Home/dash.home.controllers.js
+++ RESTServer/public/app/components/Dash/Home/dash.home.controllers.js
@@ -23,10 +23,10 @@ angular.module('DomoHome')
     /*	Controller utilizzato per la generazione e la gestione della lista di una 
     	"Collection" arbitraria
     */
-    .controller('temperaturaControllers', ['$scope','sensorFactory', function($scope,sensorFactory) {
+    .controller('temperaturaControllers', ['$scope','sensorFactory','chartLibFactory', function($scope,sensorFactory,chartLibFactory) {
 		
 			$scope.changeTemp=false;
-			console.log(angular.version);
+
 			$scope.toggleTempSwitch =  function(){
 				$scope.changeTemp=!$scope.changeTemp;
 			}
@@ -45,7 +45,49 @@ angular.module('DomoHome')
                     function( error ){
                         console.log(error);
                      }
-                  )
+                  );
+
+
+                sensorFactory.LettureSort().query({ids:'57cc7e1fa712bbee2150133d',Periodo:'201611'},
+                    function(response) {
+                       var data=[];
+                       $scope.letture =  response;
+                       
+                       for(var ele in response){
+                            var point=[];
+                            if(response[ele].hasOwnProperty('Letture')){
+                                var now = new Date(response[ele].Letture.DataPrimoInserimento);
+
+                                var now_utc = Date.UTC( now.getFullYear(), 
+                                                        now.getMonth(), 
+                                                        now.getDate(),  
+                                                        now.getHours(), 
+                                                        now.getMinutes(), 
+                                                        now.getSeconds());
+
+                                point.push(now_utc);
+                                point.push(response[ele].Letture.lettura)
+
+                                console.log(point);
+                                data.push(point);
+                            }
+                       }
+                       console.log(data);
+
+                       var cfg={
+                            title: {
+                                text: 'Temperatura interna'
+                            },
+                            subtitle: {},
+                            yAxis: {}
+                       };
+
+                       chartLibFactory.timeSeriesZoomable(data,cfg);      
+                    },
+                    function(response){
+                        console.log('Errore')
+                    }
+                );
 
     }])
 
